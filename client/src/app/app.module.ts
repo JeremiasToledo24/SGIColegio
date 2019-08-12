706a4683d7164ed60118922bee6dfd33f6bf6690
JeremiasToledo24 <jeremias.toledo@outlook.com>
2019-08-12 17:13:28 -0300

    1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaMateriasComponent } from './componentes/lista-materias/lista-materias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaMateriasComponent
   ],
   imports: [
     BrowserModule,
