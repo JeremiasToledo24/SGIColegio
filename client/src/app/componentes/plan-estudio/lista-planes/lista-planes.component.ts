29911fdb24cf17a624b3d6d3f1578c9d7bb16de9
FeerGz <fercastillo.s.1997@gmail.com>
2019-08-20 21:55:02 -0300

    Diseño HTML/CSS

--- client/src/app/componentes/plan-estudio/lista-planes/lista-planes.component.ts
+++ client/src/app/componentes/plan-estudio/lista-planes/lista-planes.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { PlanEstudioService } from 'src/app/servicios/planEstudio/plan-estudio.service';
+import {Component, OnInit} from '@angular/core';
+import {PlanEstudioService} from 'src/app/servicios/planEstudio/plan-estudio.service';
+
 class Planes {
-  id:number;
+  id: number;
   anniodelplan: string;
   Nivel: string;
   Curso: string;
   idMateria: number;
   materiacodigo: string;
   materianombre: string;
+
   constructor() {
   }
 }
+
 @Component({
   selector: 'app-lista-planes',
   templateUrl: './lista-planes.component.html',
@@ -19,19 +22,21 @@ class Planes {
 
 
 export class ListaPlanesComponent implements OnInit {
-  displayedColumns: string[] = ['id','anniodelplan','Nivel','Curso','idMateria','materiacodigo','materianombre', 'operaciones'];
+  displayedColumns: string[] = ['anniodelplan', 'Nivel', 'Curso', 'materiacodigo', 'materianombre', 'operaciones'];
   dataSource: Planes[];
-  constructor(private planService: PlanEstudioService) { }
+
+  constructor(private planService: PlanEstudioService) {
+  }
 
   ngOnInit() {
     this.planService.listarPlanes().subscribe(res => {
-      this.dataSource = res as Planes[]
-      console.log(res)
+      this.dataSource = res as Planes[];
+      console.log(res);
     });
   }
 
-  eliminarPlan(id:number){
-    this.planService.eliminarPlan(id).subscribe( res => console.log(id))
+  eliminarPlan(id: number) {
+    this.planService.eliminarPlan(id).subscribe(res => console.log(id));
   }
 
 }
