47e959acc2105c99d5ef4d5d51a32b0943690654
FeerGz <fercastillo.s.1997@gmail.com>
2019-10-25 16:17:19 -0300

    Generar factura

--- client/src/app/cobros/componentes/factura/factura.component.ts
+++ client/src/app/cobros/componentes/factura/factura.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import * as jsdPDF from 'jspdf'
+import * as jsPDF from 'jspdf'
+import * as html2canvas from '../../../../assets/js/html2canvas'
 
 @Component({
   selector: 'app-factura',
@@ -13,7 +14,7 @@ export class FacturaComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<FacturaComponent>,
     @Inject(MAT_DIALOG_DATA) public data
-    ) { }
+  ) { }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -21,15 +22,15 @@ export class FacturaComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.data)
-    const f = new Date();
-    this.fecha = (f.getDate() + '-' + (f.getMonth() + 1) + '-' + f.getFullYear());
-    
   }
 
   generaBoleta() {
-    const doc = new jsdPDF();
-    doc.fromHTML(document.getElementById('facturaPrint'),10,10);
-    doc.save('facturaPrint');
+    const pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.addHTML(document.getElementById('facturaPrint'), function () {
+      pdf.save('factura.pdf');
+    });
+    /* pdf.fromHTML(document.getElementById('facturaTest'),0,0);
+    pdf.save('test.pdf') */
   }
 
   // Cerrar dialog
