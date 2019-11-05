8f70d35654d5e0ca3bd750e73a406de997b03dde
Fernando G. Castillo S <fercastillo.s.1997@gmail.com>
2019-11-05 18:02:09 -0300

    fix Factura

--- client/src/app/cobros/componentes/factura/factura.component.ts
+++ client/src/app/cobros/componentes/factura/factura.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import * as jsdPDF from 'jspdf'
+import * as jsPDF from 'jspdf'
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-factura',
@@ -13,7 +14,7 @@ export class FacturaComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<FacturaComponent>,
     @Inject(MAT_DIALOG_DATA) public data
-    ) { }
+  ) { }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -23,13 +24,13 @@ export class FacturaComponent implements OnInit {
     console.log(this.data)
     const f = new Date();
     this.fecha = (f.getDate() + '-' + (f.getMonth() + 1) + '-' + f.getFullYear());
-    
   }
 
   generaBoleta() {
-    const doc = new jsdPDF();
-    doc.fromHTML(document.getElementById('facturaPrint'),10,10);
-    doc.save('facturaPrint');
+    var pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.addHTML(document.getElementById('to-pdf'), function () {
+      pdf.save('factura.pdf');
+    });
   }
 
   // Cerrar dialog
